c56e9ad85041e6d63ef31aea7f9034a48452b4cb
ron pod <ron.podkoshin@gmail.com>
2023-09-10 10:03:46 -0500

    hello

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ronald Podkoshin";
+    int age = 32;
+    String state = "New York";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year = 1991;
+    String month = "May";
+    boolean weekend = false;
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
